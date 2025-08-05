be9244944322f56e26a93eebe7ddf15b9afb5b35
JAEHYUN GO <gojaehyeonstudio@gmail.com>
2025-08-05 19:45:45 +0900

    feat: 프로젝트 설정

---
+++ jarago/jarago/jaragoApp.swift
@@ -0,0 +1,17 @@
+//
+//  jaragoApp.swift
+//  jarago
+//
+//  Created by Gojaehyun on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct jaragoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jarago/jaragoTests/jaragoTests.swift
@@ -0,0 +1,17 @@
+//
+//  jaragoTests.swift
+//  jaragoTests
+//
+//  Created by Gojaehyun on 8/5/25.
+//
+
+import Testing
+@testable import jarago
+
+struct jaragoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
